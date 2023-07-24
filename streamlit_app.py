ce8e14f5de9ed07de40e5558bf674656eb4665e0
basim103 <137525611+basim103@users.noreply.github.com>
2023-07-24 14:00:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
